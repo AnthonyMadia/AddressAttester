0722405e7e21eaebcacb3543c1414b508953ccc7
AnthonyMadia <aamadia24@gmail.com>
2023-02-10 12:10:45 -0500

    clean up

--- packages/circuits/scripts/circuits.mjs
+++ packages/circuits/scripts/circuits.mjs
@@ -3,6 +3,9 @@ import { SMT_DEPTH, SMT_ARITY } from "../config/index";
 export const ptauName = "powersOfTau28_hez_final_17.ptau";
 
 export const circuitContents = {
-  // todo: figure out the correct inputs
+  // todo: figure out the correct inputs 
+  // need SMT proof, and spartan ecdsa proof(s) - not sure
+  privkeyToAddr: `pragma circom 2.0.0; include "../circuits/eth_addr.circom"; \n\ncomponent main { public [ start_root ] } = AggregateEpochKeys(${EPOCH_TREE_DEPTH}, ${EPOCH_TREE_ARITY}, ${AGGREGATE_KEY_COUNT});`,
+  privkeyToAddr: `pragma circom 2.0.0; include "../circuits/eth_addr.circom"; \n\ncomponent main { public [ start_root ] } = AggregateEpochKeys(${EPOCH_TREE_DEPTH}, ${EPOCH_TREE_ARITY}, ${AGGREGATE_KEY_COUNT});`,
   privkeyToAddr: `pragma circom 2.0.0; include "../circuits/eth_addr.circom"; \n\ncomponent main { public [ start_root ] } = AggregateEpochKeys(${EPOCH_TREE_DEPTH}, ${EPOCH_TREE_ARITY}, ${AGGREGATE_KEY_COUNT});`,
 };
