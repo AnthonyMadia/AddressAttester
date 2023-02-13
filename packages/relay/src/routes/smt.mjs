a058fe6ea5b444aea712d1606b4abcb824491297
AnthonyMadia <aamadia24@gmail.com>
2023-02-13 16:14:36 -0500

    add: initial queue for waiting for proof to verify

--- packages/relay/src/routes/smt.mjs
+++ packages/relay/src/routes/smt.mjs
@@ -12,28 +12,45 @@ const depth = 26;
 const zeroHash = 0;
 
 const tree = new SparseMerkleTree(depth, zeroHash, arity);
+const queue = [];
+let isVerifying = false;
 
 export default ({ app, db, synchronizer }) => {
   app.post("/api/register", async (req, res) => {
     try {
-      // todo: make zkproof setting the leaf at their address index to 1
       // todo: grab address from frontend or somewhere
-      // const { ethAddress } = req.body;
-      // add eth address to the SMT
-      tree.update(ethAddress, 1);
-      // send root to smart contract
-      let root = tree.root;
-      // post root on chain
-      const appContract = new ethers.Contract(
-        ATTESTERADD_ADDRESS,
-        UnirepApp.abi
-      );
-      const calldata = appContract.interface.encodeFunctionData(
-        "setRoot",
-        root
-      );
+      const { ethAddress } = req.body;
+      queue.push(ethAddress);
+      verifyAddress();
     } catch (error) {
       res.status(500).json({ error });
     }
   });
+
+  async function verifyAddress() {
+    if (isVerifying || queue.length === 0) {
+      return;
+    }
+
+    isVerifying = true;
+    const ethAddress = queue.shift();
+    // todo: add zk proof here to check if address is in SMT
+    // todo: ====
+    // add eth address to the SMT
+    tree.update(ethAddress, 1);
+    // send root to smart contract and post root on chain
+    let root = tree.root;
+    const appContract = new ethers.Contract(ATTESTERADD_ADDRESS, UnirepApp.abi);
+    const calldata = appContract.interface.encodeFunctionData("setRoot", root);
+    // send the transaction to the blockchain
+    const hash = await TransactionManager.queueTransaction(
+      appContract.address,
+      calldata
+    );
+
+    res.json({ hash });
+
+    isVerifying = false;
+    verifyAddress();
+  }
 };
