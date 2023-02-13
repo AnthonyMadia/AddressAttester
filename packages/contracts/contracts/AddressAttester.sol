4c48d2b75d538fd9b43b293297417997fccb6d16
AnthonyMadia <aamadia24@gmail.com>
2023-02-13 11:19:14 -0500

    add: polish contract

--- packages/contracts/contracts/AddressAttester.sol
+++ packages/contracts/contracts/AddressAttester.sol
@@ -9,40 +9,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract AddressAttester is Ownable {
 
     // root stored onchain
-    uint SMTRoot;
+    bytes32 public root;
 
     Unirep public unirep;
 
-    mapping (address => bool) public registeredAddresses;
-
-    constructor(Unirep _unirep, uint256 _epochLength) {
+    constructor(Unirep _unirep, uint256 _epochLength, bytes32 _root) {
         // set unirep address
         unirep = _unirep;
+
+        // set SMT root
+        root = _root;
 
         // sign up as an attester
         unirep.attesterSignUp(_epochLength);
     }
 
     
-    function getRoot() public view returns (uint) {
-      return SMTRoot;
-    }
-
-    // strictly reserved for owner of contract
-    function setRoot(uint _SMTRoot) public onlyOwner (){
-      SMTRoot = _SMTRoot;
-    } 
-
-    // insert into the tree -- index is the Address and value is 1
-    function register(address addr) public {
-        // check if Address hasn't been registered before and only the address owner can register it
-        require(!registeredAddresses[addr]); 
-        require(msg.sender == addr);
-        registeredAddresses[addr] = true;
-    }
-
-    function verify(address addr) public view returns (bool) {
-        return registeredAddresses[addr];
+    function getRoot() public view returns (bytes32) {
+      return root;
     }
 
 
@@ -52,23 +36,6 @@ contract AddressAttester is Ownable {
         uint256[8] memory proof
     ) public {
         unirep.userSignUp(publicSignals, proof);
-    }
-
-    // submit attestations (fields may change with https://github.com/Unirep/Unirep/issues/285)
-    function submitAttestation(
-        uint256 targetEpoch,
-        uint256 epochKey,
-        uint256 posRep,
-        uint256 negRep,
-        uint256 graffiti
-    ) public {
-        unirep.submitAttestation(
-            targetEpoch,
-            epochKey,
-            posRep,
-            negRep,
-            graffiti
-        );
     }
 
     // todo: Verify proofs when circuits are built
