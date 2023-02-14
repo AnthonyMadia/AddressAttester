1d1caf58a566df57c919a1de4a4e525849fb9afd
AnthonyMadia <aamadia24@gmail.com>
2023-02-14 17:04:42 -0500

    add: update SMT depth and arity

--- packages/circuits/config/index.ts
+++ packages/circuits/config/index.ts
@@ -1,7 +1,7 @@
 import { SparseMerkleTree, hash4 } from "@unirep/utils";
 
-export const SMT_DEPTH = 26;
-export const SMT_ARITY = 12;
+export const SMT_DEPTH = 43;
+export const SMT_ARITY = 14;
 
 export const defaultSMTLeaf = hash4([0, 0, 0, 0]);
 const smt = new SparseMerkleTree(SMT_DEPTH, defaultSMTLeaf, SMT_ARITY);
