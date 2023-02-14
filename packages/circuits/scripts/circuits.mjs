d9b298e020ff541718356e412bcbc563325fda3c
AnthonyMadia <aamadia24@gmail.com>
2023-02-14 14:10:02 -0500

    merging setup only

--- packages/circuits/scripts/circuits.mjs
+++ packages/circuits/scripts/circuits.mjs
@@ -2,7 +2,4 @@ import { SMT_DEPTH, SMT_ARITY } from "../config/index";
 
 export const ptauName = "powersOfTau28_hez_final_17.ptau";
 
-export const circuitContents = {
-  pubKeyMembership: `pragma circom 2.0.0; include "../circuits/eff_ecdsa_membership/pubkey_membership.circom"; \n\ncomponent main { public[ root, Tx, Ty, Ux, Uy ]} = PubKeyMembership(${SMT_DEPTH}, ${SMT_ARITY});`,
-  addrMembership: `pragma circom 2.0.0; include "../circuits/eff_ecdsa_membership/addr_membership.circom"; \n\ncomponent component main { public[ root, Tx, Ty, Ux, Uy ]} = AddrMembership(${SMT_DEPTH}, ${SMT_ARITY});`,
-};
+export const circuitContents = {};
