01431bda9b66338fdaa4640fa511f828697340f4
AnthonyMadia <aamadia24@gmail.com>
2023-02-16 14:22:50 -0500

    fix: remove circuits for v0

--- packages/circuits/README.md
+++
@@ -1,7 +0,0 @@
-# Circuits
-
-Directory for circuits used in the Address Atteser
-
-## Acknowledgements
-
-Leveraging ecdsa circuits from 0xParc and PersonaeLabs
--- packages/circuits/config/index.ts
+++
@@ -1,8 +0,0 @@
-import { SparseMerkleTree, hash4 } from "@unirep/utils";
-
-export const SMT_DEPTH = 43;
-export const SMT_ARITY = 14;
-
-export const defaultSMTLeaf = hash4([0, 0, 0, 0]);
-const smt = new SparseMerkleTree(SMT_DEPTH, defaultSMTLeaf, SMT_ARITY);
-export const EMPTY_SMT_TREE_ROOT = smt.root.toString();
