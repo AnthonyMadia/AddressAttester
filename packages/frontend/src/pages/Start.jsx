73620d1075f9d3fa35e4b7b19b768e674b06a202
AnthonyMadia <aamadia24@gmail.com>
2023-02-17 16:06:21 -0500

    clean up

--- packages/frontend/src/pages/Start.jsx
+++ packages/frontend/src/pages/Start.jsx
@@ -24,12 +24,7 @@ export default observer(() => {
                 <img src={require('../../public/hummingbird.svg')} alt="hummingbird at a flower"/>
             </div>
             <div className='content'>
-                <div style={{fontSize: '70px', fontWeight: '600'}}>Congratulations</div>
-                <div className='attester'>
-                    <div style={{marginRight: '12px'}}>You have created a new UniRep attester </div>
-                    <Tooltip text='Attesters define their own reputation systems and are able to attest to users, giving them reputation and graffiti.'/>
-                </div>
-                <p>Clicking 'Join' adds a user to this attester's membership group.</p>
+                <p>Click 'Join' to sign up to the Address Attester's membership group.</p>
                 <div className='join'>
                     {!userContext.hasSignedUp ? (
                         <Button onClick={() => userContext.signup()}>Join<span style={{marginLeft: '12px'}}><img src={require('../../public/arrow.svg')} alt="right arrow"/></span></Button>
@@ -40,10 +35,11 @@ export default observer(() => {
                         </div>
                     )}
                 </div>
-                <p>After joining, the member can interact with reputation in the attester's application. </p>
-                <p>Customize this landing page to onboard new users to your app.</p>
             </div>
         </>
 
     )
 })
+
+// userContext.signup()
+// <Button onClick={() => userContext.signup()}>Join<span style={{marginLeft: '12px'}}><img src={require('../../public/arrow.svg')} alt="right arrow"/></span></Button>
