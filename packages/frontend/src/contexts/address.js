9536cbe7a74868a0d863627ced41481cd2bc2c2d
AnthonyMadia <aamadia24@gmail.com>
2023-03-02 12:33:27 -0500

    chore(frontend): refactor address store

--- packages/frontend/src/contexts/address.js
+++ packages/frontend/src/contexts/address.js
@@ -5,6 +5,7 @@ import { SERVER } from '../config'
 export default class Address {
   hasSignedUp = false
   address = null
+  signature = null
 
   constructor() {
     makeAutoObservable(this)
@@ -17,18 +18,24 @@ export default class Address {
     console.log(this.address)
   }
 
+  setSignature(signature) {
+    this.signature = signature
+    console.log(this.signature)
+  }
+
   // prove control of an address and sign some text
-  async proveAddressData(text) {
+  async proveAddressData() {
     const addrIndex = this.addressIndex(BigInt(this.address))
     if (addrIndex === -1) {
       throw new Error('You are not authorized to chat here')
     }
     const addressTreeProof = this.addressTree.createProof(addrIndex)
-    // take the upper 250 bits to fit into a bn128 field element
-    const hash = ethers.utils.keccak256(
-      '0x' + Buffer.from(text, 'utf8').toString('hex')
-    )
-    const sig_data = BigInt(hash) >> BigInt(6)
+    // // take the upper 250 bits to fit into a bn128 field element
+    // const hash = ethers.utils.keccak256(
+    //   '0x' + Buffer.from(text, 'utf8').toString('hex')
+    // )
+    // todo: we have signatures hash already from signing a message
+    const sig_data = BigInt(signature) >> BigInt(6)
     const data = await this.userState.getData()
     const stateTree = await this.userState.sync.genStateTree(0)
     const index = await this.userState.latestStateTreeLeafIndex()
